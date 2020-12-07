5700a65db8fdf576d53ac0bb49a3a1a732c28638
Pedro Castilho <cstl.px@gmail.com>
2020-12-07 14:53:30 -0300

    Added a few more list utils and tests

--- src/Overture.hs
+++ src/Overture.hs
@@ -2,7 +2,12 @@ module Overture
     ( takeEvery
     , takeEveryLast
     , withIndex
+    , chunksOf
     , replace
+    , replaceWhere
+    , replicateElems
+    , sum'
+    , product'
     , (.)
     , (&)
     , ($)
@@ -10,33 +15,60 @@ module Overture
     , (<&>)
     , (<<<)
     , (>>>)
-    , ($>>>)
-    , (<<<$)
+    , ($>>)
+    , (<<$)
     ) where
 
+import Data.List (foldl')
 import Data.Function ((&))
 import Data.Functor ((<&>))
-import Control.Arrow ((<<<), (>>>))
 
-(<<<$) :: Functor f => (b -> c) -> (a -> f b) -> a -> f c
-f <<<$ g = fmap f . g
+(<<<) :: (b -> c) -> (a -> b) -> a -> c
+(<<<) = (.)
 
-($>>>) :: Functor f => (a -> f b) -> (b -> c) -> a -> f c
-f $>>> g = fmap g . f
+(>>>) :: (a -> b) -> (b -> c) -> a -> c
+(>>>) = flip (.)
+
+(<<$) :: Functor f => (b -> c) -> (a -> f b) -> a -> f c
+f <<$ g = fmap f . g
+
+($>>) :: Functor f => (a -> f b) -> (b -> c) -> a -> f c
+f $>> g = fmap g . f
 
 replace :: Eq a => a -> a -> [a] -> [a]
-replace el _   []     = []
-replace el rep (x:xs)
-  | x == el   = rep : (replace el rep xs)
-  | otherwise = x : (replace el rep xs)
+replace x y = replaceWhere (== x) (const y)
+
+replaceWhere :: (a -> Bool) -> (a -> a) -> [a] -> [a]
+replaceWhere _             _         []     = []
+replaceWhere shouldReplace transform (x:xs)
+  | shouldReplace x = transform x : (replaceWhere shouldReplace transform xs)
+  | otherwise       = x : (replaceWhere shouldReplace transform xs)
+  
 
 takeEvery :: Int -> [a] -> [a]
 takeEvery n []     = []
 takeEvery n (x:xs) = x : (takeEvery n (drop (n-1) xs))
-  
+
 takeEveryLast :: Int -> [a] -> [a]
 takeEveryLast n list =
   takeEvery n (drop (n-1) list)
 
 withIndex :: [a] -> [(Int, a)]
 withIndex = zip [0..]
+
+chunksOf :: Int -> [a] -> [[a]]
+chunksOf n l =
+  let (prefix, suffix) = splitAt n l
+  in case suffix of
+    [] -> [prefix]
+    _  -> prefix : (chunksOf n suffix)
+
+sum' :: Num a => [a] -> a
+sum' = foldl' (+) 0
+
+product' :: Num a => [a] -> a
+product' = foldl' (*) 1
+
+replicateElems :: Int -> [a] -> [a]
+replicateElems _ []     = []
+replicateElems n (x:xs) = replicate n x ++ (replicateElems n xs)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,9 @@
+-- TODO Add tests for <<<, >>>, <<$ and $>>
+{-# LANGUAGE ScopedTypeVariables #-}
+
 import Test.Tasty (TestTree, defaultMain, testGroup)
 import Test.Tasty.HUnit (Assertion, testCase, assertBool, (@?=))
-import Test.Tasty.QuickCheck (testProperty)
+import Test.Tasty.QuickCheck (testProperty, (==>), withMaxSuccess)
 
 import qualified Overture
 
@@ -11,7 +14,7 @@ main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "All tests" 
+tests = testGroup "All tests"
   [ testCase "takeEvery 2 from integers is same as filter even" $
       let integers = [0..100]
       in Overture.takeEvery 2 integers @?= filter even integers
@@ -24,4 +27,34 @@ tests = testGroup "All tests"
       let integers = [0..100]
       in assertBool "not all pairs match" $
            and $ (uncurry (==)) <$> (Overture.withIndex integers)
+  , testProperty "product' and product are equivalent" $
+      \(numberList :: [Integer]) ->
+        product numberList == Overture.product' numberList
+  , testProperty "sum' and sum are equivalent" $
+      \(nums :: [Integer]) ->
+        sum nums == Overture.sum' nums
+  , testProperty "chunksOf empty list produces empty list" $
+      \n ->
+        Overture.chunksOf n ([] :: [Integer]) == [[]]
+  , testProperty "chunksOf splits list into chunks" $
+      \n (nums :: [Integer]) ->
+        n > 0 && not (null nums) ==>
+          length (Overture.chunksOf n nums) == ceiling ((fromIntegral $ length nums) / (fromIntegral n))
+  , testProperty "chunksOf leaves maximal chunk at end" $
+      \n (nums :: [Integer]) ->
+        n > 0 && not (null nums) ==>
+          if length nums `mod` n == 0
+          then length (last (Overture.chunksOf n nums)) == n
+          else length (last (Overture.chunksOf n nums)) == length nums `mod` n
+  , testProperty "chunksOf preserves original list" $
+      \n (nums :: [Integer]) ->
+        n > 0 ==>
+          concat (Overture.chunksOf n nums) == nums
+  , testProperty "replace replaces all instances" $
+      \n x (y::Int) -> Overture.replace x y (replicate n x) == replicate n y
+  , testProperty "replace does not replace unduly" $
+      \n z x (y::Int) -> x /= y ==>
+        Overture.replace x z (replicate n y) == replicate n y
+  , testCase "replaceWhere even" $
+      (Overture.replaceWhere even (subtract 1) [1..100]) @?= (Overture.replicateElems 2 [1,3..100])
   ]
